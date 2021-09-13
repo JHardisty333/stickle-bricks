92ac8daddd21b172cf1d625d9f81daca726d3e5b
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-09-12 22:14:31 -0600

    Added images to json file

--- server/routes/api/test.js
+++ server/routes/api/test.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
-const axios = require('axios');
 const OAuth =require('oauth');
 const items = require('../../utils/items.json');
 const fs = require('fs');
-const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require('constants');
 
 let itemsArray = items;
-const image = [];
+let i = 0;
 
 router.get('/images', (req, res) => {
     const oauth = new OAuth.OAuth(
@@ -19,48 +17,54 @@ router.get('/images', (req, res) => {
         'HMAC-SHA1'
     );
     let obj;
-    for (let i = 0; i < items.length; i++) {
-        console.log(i)
-        let itemType;
-        switch (items[i].ITEMTYPE) {
-            case 'P':
-                itemType = "PART";
-                break;
-            case 'M':
-                itemType = "MINIFIG";
-                break;
-            case 'S':
-                itemType = "SET";
-                break;
-            case 'B':
-                itemType = "BOOK";
-                break;
-            case 'G':
-                itemType = 'GEAR';
-                break;
-            case 'C':
-                itemType = "CATALOG";
-                break;
-            case 'I':
-                itemType = "INSTRUCTION";
-                break;
-            case 'U':
-                itemType = "UNSORTED_LOT";
-                break;
-            case 'O':
-                itemType = "ORIGINAL_BOX";
-                break;
-        }
-        //aka fetch but with oath
-        oauth.get(`https://api.bricklink.com/api/store/v1/items/${itemType}/${items[i].ITEMID}`,
-            'E027EE7A5E224096881DB12161248CB5',
-            'ED294BAB69D44A83A3EAB65287D12B73',
-            (error, data) => {
-                if (error) console.log(error);
-                obj = JSON.parse(data);
-                image.push(obj.data.image_url);
-            });
+    let itemType;
+    console.log(i)
+    switch (items.items[i].ITEMTYPE) {
+        case 'P':
+            itemType = "PART";
+            break;
+        case 'M':
+            itemType = "MINIFIG";
+            break;
+        case 'S':
+            itemType = "SET";
+            break;
+        case 'B':
+            itemType = "BOOK";
+            break;
+        case 'G':
+            itemType = 'GEAR';
+            break;
+        case 'C':
+            itemType = "CATALOG";
+            break;
+        case 'I':
+            itemType = "INSTRUCTION";
+            break;
+        case 'U':
+            itemType = "UNSORTED_LOT";
+            break;
+        case 'O':
+            itemType = "ORIGINAL_BOX";
+            break;
     }
+    //aka fetch but with oath
+    oauth.get(`https://api.bricklink.com/api/store/v1/items/${itemType}/${items.items[i].ITEMID}`,
+        'E027EE7A5E224096881DB12161248CB5',
+        'ED294BAB69D44A83A3EAB65287D12B73',
+        (error, data) => {
+            if (error) console.log(error);
+            obj = JSON.parse(data);
+            itemsArray.items[i].IMAGE = obj.data.image_url
+            i++;
+            let json = JSON.stringify(itemsArray);
+            fs.writeFile('./utils/items.json', json, (err) => {
+                if (err) throw err;
+                console.log('Update successful for index ' + i + '.')
+            });
+        });
+        res.send(obj);
+        
 });
 
 
