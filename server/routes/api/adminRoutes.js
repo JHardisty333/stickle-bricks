776053750a89778adb17fdbd054009c5f5008b90
Kelley Worthington <kelleymarne@gmail.com>
2021-09-23 20:30:46 -0600

    pulling

--- server/routes/api/adminRoutes.js
+++ server/routes/api/adminRoutes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { 
-    contactAdmin
-} = require('../../controllers/adminControllers');
-const { verifyToken } = require('../../utils/auth');
+// const { 
+//     contactAdmin
+// } = require('../../controllers/adminControllers');
+// const { verifyToken } = require('../../utils/auth');
 
-router.route('/contact')
-.post(verifyToken, contactAdmin)
+// router.route('/contact')
+// .post(verifyToken, contactAdmin)
 
 module.exports = router;
