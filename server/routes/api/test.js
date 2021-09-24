2c8cf30f3669a6553dd5ebd13d673b89a5176c5b
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-09-24 14:14:49 -0600

    added testing route

--- server/routes/api/test.js
+++ server/routes/api/test.js
@@ -67,5 +67,66 @@ router.get('/images', (req, res) => {
         
 });
 
+router.get('/test', async (req, res) => {
+    let itemsList = [];
+    for (let i = 0; i < ITEMS.length; i++) {
+        console.log(i, ITEMS[i].LOTID)
+        let colorObj = COLORS.find(({ color_id }) => color_id === parseInt(ITEMS[i].COLOR));
+        let categoryObj = CATEGORIES.find(({ category_id }) => category_id === parseInt(ITEMS[i].CATEGORY));
+        let itemType;
+        switch (ITEMS[i].ITEMTYPE) {
+            case 'P':
+                itemType = "PART";
+                break;
+            case 'M':
+                itemType = "MINIFIG";
+                break;
+            case 'S':
+                itemType = "SET";
+                break;
+            case 'B':
+                itemType = "BOOK";
+                break;
+            case 'G':
+                itemType = 'GEAR';
+                break;
+            case 'C':
+                itemType = "CATALOG";
+                break;
+            case 'I':
+                itemType = "INSTRUCTION";
+                break;
+            case 'U':
+                itemType = "UNSORTED_LOT";
+                break;
+            case 'O':
+                itemType = "ORIGINAL_BOX";
+                break;
+        }
+        let obj = {
+            productName: ITEMS[i].DESCRIPTION,
+            productId: ITEMS[i].ITEMID,
+            colorId: colorObj ? colorObj.color_id : 0,
+            colorName: colorObj ? colorObj.color_name : 0,
+            colorCode: colorObj ? colorObj.color_code : 0,
+            itemType: itemType,
+            price: ITEMS[i].PRICE,
+            quantity: ITEMS[i].QTY,
+            image: [ITEMS[i].IMAGE],
+            itemWeight: parseInt(ITEMS[i].ITEMWEIGHT),
+            active: true,
+            categoryId: categoryObj.category_id,
+            categoryName: categoryObj.category_name
+        }
+        itemsList.push(obj);
+    }
+    const jsonData = JSON.stringify(itemsList);
+    writeFile('utils/itemSeed.json', jsonData, (err) => {
+        if (err) throw err;
+        console.log('File Created Successfully');
+        res.sendStatus(200);
+    });
+});
+
 
 module.exports = router;
