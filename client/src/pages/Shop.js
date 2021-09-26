02b693bde17f52870b75a8c49bbd0f8667078299
Justin Berg <justinberg@Justins-MBP.localdomain>
2021-09-26 16:36:30 -0600

    shop

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-function Shop() {
-    
-}
+const Shop = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'Right',
+                alignItems: 'Right',
+                height: '100vh'
+            }}
+        >
+            <h1></h1>
+        </div>
+    );
+};
 
 export default Shop;
