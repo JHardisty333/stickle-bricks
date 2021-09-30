faa5fef2bff5fff2effb30859af07f85a2d9451c
Kelley Worthington <kelleymarne@gmail.com>
2021-09-30 13:19:13 -0600

    pulling current

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/sticklebrick', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/sticklebricks', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     
