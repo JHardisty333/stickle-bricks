ab10b3bb47b054ec6c23ef5de2ac1bbdb03b0bff
Justin Berg <justinberg@Justins-MBP.localdomain>
2021-09-30 01:22:51 -0600

    update about

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import {addCartApi, itemApi} from '../utils/api'
+// import React from 'react';
+// import {addCartApi, itemApi} from '../utils/api'
 
 
-function Login() {
-    function addToCart() {
-        //get item info from dom,
-        const JWT = localStorage.JWT
-        addCartApi(JWT, ItemId, Quantity)
-    }
-    const items = await itemApi()
-    return (
-        <>
-        <button type="button" onClick={addToCart}>
-        </button>
-        {{items.map(item => (
-            <div id={item._id}></div>
-        ))}}
-        </>
-    );
-}
+// // function Login() {
+// //     function addToCart() {
+// //         //get item info from dom,
+// //         const JWT = localStorage.JWT
+// //         addCartApi(JWT, ItemId, Quantity)
+// //     }
+// //     const items = await itemApi()
+// //     return (
+// //         <>
+// //         <button type="button" onClick={addToCart}>
+// //         </button>
+// //         {{items.map(item => (
+// //             <div id={item._id}></div>
+// //         ))}}
+// //         </>
+// //     );
+// // }
 
-export default Login;
+// export default Login;
