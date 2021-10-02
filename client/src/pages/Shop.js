98269c53b3a1f248a4c13bc7ad235b69be320d90
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-10-02 17:50:42 -0600

    still working on store page

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -6,56 +6,53 @@ import {
     ModalBody,
     ModalFooter,
     Container,
-    Pagination,
-    PaginationItem,
-    PaginationLink,
     Spinner,
     Col,
     Row
 } from 'reactstrap';
-import { itemsApi } from "../utils/api";
+import { itemsApi, itemApi } from "../utils/api";
 
 
 const Shop = () => {
     //modal controls
     const [modal, setModal] = useState(false);
     const toggle = () => setModal(!modal);
+    // const timer = setInterval(() => console.log(pageIndex), 10000)
 
-    let totalItems = []; //current array of items 
+    const [totalItems, setTotalItems] = useState([]) //current array of items 
     const [Items, setItems] = useState((<Spinner color="dark" className="my-5 p-4 mx-auto" />)); // current items displayed on page
-    const [pageIndex, setPageIndex] = useState([0, 49]);
+    const [pageIndex, setPageIndex] = useState([0, 49])
     const [maxIndex, setMaxIndex] = useState(1);
     const [currentIndex, setCurrentIndex] = useState(1)
     const [modalItem, setModalItem] = useState({
-        "_id": "6157aeb54ed011c14c2ca42e",
-        "productName": "Vehicle Tipper Bed Small",
-        "productId": "2512",
-        "colorId": 9,
-        "colorName": "Light Gray",
-        "colorCode": "9c9c9c",
-        "itemType": "PART",
+        "_id": "",
+        "productName": "",
+        "productId": "",
+        "colorId": 0,
+        "colorName": "",
+        "colorCode": "",
+        "itemType": "",
         "price": {
-            "$numberDecimal": "0.16"
+            "$numberDecimal": "0.00"
         },
-        "quantity": 2,
+        "quantity": 0,
         "image": [
-            "//img.bricklink.com/PL/2512.jpg"
+            ""
         ],
         "condition": "Used",
         "itemWeight": {
-            "$numberDecimal": "3"
+            "$numberDecimal": "0.00"
         },
         "active": true,
         "featured": false,
-        "categoryId": 72,
-        "date_added": "2021-10-02T00:58:29.119Z",
-        "id": "6157aeb54ed011c14c2ca42e"
+        "categoryId": 0,
+        "date_added": "0",
+        "id": ""
     });
 
-
-    function pagination(e) {
-        const indexType = e.target.value;
-        console.log(e.target);
+    async function pagination(e) { 
+        const indexType = e.target.id;
+        setItems((<Spinner color="dark" className="my-5 p-4 mx-auto" />))
         if (indexType === 'start') {
             setCurrentIndex(1);
             setPageIndex([0, 49]);
@@ -73,7 +70,7 @@ const Shop = () => {
         }
 
         if (indexType === 'minus') {
-            if (!pageIndex[0] === 0) {
+            if (pageIndex[0] !== 0) {
                 let index = [];
                 for (let i = 0; i < pageIndex.length; i++) {
                     index.push(pageIndex[i] - 50);
@@ -91,26 +88,25 @@ const Shop = () => {
             setPageIndex(index);
             setCurrentIndex(maxIndex);
         }
-
+        console.log(pageIndex)
         const pageItems = totalItems.slice(pageIndex[0], pageIndex[1]);
-        setItems(pageItems.map((item, index) => (
-            <div key={item._id} className='itemStyle'>
-                <img src={item.image[0]} alt={item.productName} id={index} onClick={(e) => productClick(e)} style={{}} />
+        setItems(pageItems.map((item) => (
+            <Col sm={4} key={item._id} className='itemStyle'>
+                <img src={item.image[0]} alt={item.productName} id={item._id} onClick={productClick} style={{ "maxWidth": "100%", "height": "50%" }} />
                 <p>{item.productName}</p>
                 <p>{item.condition}</p>
                 <p>{parseFloat(item.price.$numberDecimal)}</p>
-            </div>
+            </Col>
         )))
-
     }
 
 
 
-    const productClick = (event) => { // to open modal
-        console.log(event.target)
-        setModalItem(totalItems[event.target.id])// this should reference the item index
-        console.log(event.target.id)
-        console.log(totalItems)
+    const productClick = async (event) => { // to open modal
+        const response = await itemApi(event.target.id)
+        if (!response.ok) alert('an error has occurred')
+        const item = await response.json();
+        setModalItem(item)// this should reference the item index
         toggle()
     }
 
@@ -119,15 +115,13 @@ const Shop = () => {
     async function fetchData() {
         const response = await itemsApi()
         if (!response.ok) alert('an error has occurred')
-        const items = await response.json()
-
-        totalItems = items;
+        const items = await response.json();
+        setTotalItems(items);
         setMaxIndex((Math.ceil(items.length / 50)))
         const pageItems = items.slice(pageIndex[0], pageIndex[1]);
-        console.log(items)
-        setItems(pageItems.map((item, index) => (
+        setItems(pageItems.map((item) => (
             <Col sm={4} key={item._id} className='itemStyle'>
-                <img src={item.image[0]} alt={item.productName} id={index} onClick={productClick} style={{"maxWidth":"100%", "height": "50%"}} />
+                <img src={item.image[0]} alt={item.productName} id={item._id} onClick={productClick} style={{"maxWidth":"100%", "height": "50%"}} />
                 <p>{item.productName}</p>
                 <p>{item.condition}</p>
                 <p>{parseFloat(item.price.$numberDecimal)}</p>
@@ -141,7 +135,7 @@ const Shop = () => {
 
     return (
         <Container>
-            <Row>
+            <Row id="top">
                 <input type="text"></input>
                 {/* searchbar and sort options */}
             </Row>
@@ -172,12 +166,31 @@ const Shop = () => {
                 </Modal>
             </div>
 
-            <Row>
-
+            <Row className="d-flex">
+                <a href="#top">
+                    <button id='start' onClick={(e) => pagination(e)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="3em" id='start' fill="currentColor" className="" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+                            <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+                        </svg>
+                    </button>
+                </a>
+                <a href="#top">
+                    <button id='minus' onClick={(e) => pagination(e)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="3em" fill="currentColor" id='minus' className="" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+                        </svg>
+                    </button>
+                </a>
+                <a href="#top">
+                    <button id='plus' onClick={(e) => pagination(e)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="3em" fill="currentColor" id='plus' className="" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
+                        </svg>
+                    </button>
+                </a>
             </Row>
-            <button>
-            
-            </button>
+
             {/* <Pagination aria-label="Page navigation example">
                 <PaginationItem>
                     <PaginationLink first value='start' onClick={(e) => pagination(e)} />
