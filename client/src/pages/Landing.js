fe98fc046068a9b81821f4ffd97b23163097e888
JHardisty333 <jessica1990_37@hotmail.com>
2021-10-02 17:50:57 -0600

    cleaned up footer and nav

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -7,13 +7,13 @@ const Landing = () => {
   <div className="landing-body" style={{}}>
     <section className="landing">
       <div className="greeting">
-        <div className="greeting__text">
+        <div className="greeting-text">
           <span className>Welcome to StickleBrick’s!
             <br />We strive to offer all fans of <br />LEGO© both young and old
             <br />a quality place to find all the greatest sets, <br />minifigs, and
             even the last piece<br />needed to finish your MOC!</span>
         </div>
-        <button type="button" id="Deals">Deal of the Day</button>
+        {/* <button className="landingButton" type="button" id="Deals">Shop Now</button> */}
       </div>
     </section>
   </div>
