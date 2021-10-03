da51e60c6f5a41bc4f0124c60eb5288f5914c197
JHardisty333 <jessica1990_37@hotmail.com>
2021-10-03 12:28:35 -0600

    shop styling

--- test-reference/JustinsSanity/components/SliderData.js
+++
@@ -1,8 +0,0 @@
-export const SliderData = [
-{
-    image: "test-reference/JustinsSanity/static/img/legoHands.png"
-},
-{
-    image: "/Users/justinberg/Desktop/Bootcamp/Project_3/stickle-bricks/test-reference/JustinsSanity/static/img/legoRed.png"
-}
-]
