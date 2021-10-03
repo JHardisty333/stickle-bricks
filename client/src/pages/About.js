65c0234e851b549bdef7798de7c94d482302e93c
JHardisty333 <jessica1990_37@hotmail.com>
2021-10-02 22:03:30 -0600

    style.css updates

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,21 +3,23 @@ import React from 'react';
 const About = () => {
   return (
     <div className="about-body">
-      <section className="about"> 
-        <div className="aboutus">
-          <div className="about_us">
-            <span className>
+      <div className="about-body-pic">
+      <section className="about">
+        <div className="about-us">
+          <div className="about-text">
+            <span>
               <h1>About Us</h1>
               StickleBrick's first began their journey in August 2006. It was an old childhood Lego set missing a few key pieces that started the internet search for replacements which first brought us to Bricklink.
               It wasn't long before the AFOL passion kicked in to high gear and buying missing pieces turned in to a mission to return the favor and the StickleBrick's BrickLink Lego store was opened for business.
               StickleBrick's is a genuine one stop shop specializing in themed sets, unusual finds, and bulk brick purchases.
-              </span>
+            </span>
           </div>
         </div>
       </section>
+      </div>
     </div>
   );
 };
-  
+
 export default About;
 
