227165d02c4af4268d3ee6e76708c778f329884f
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-10-03 22:22:52 -0600

    removing node_modules

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/sticklebricks', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    
 });
 
 module.exports = mongoose.connection;
